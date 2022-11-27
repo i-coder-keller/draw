3d198eed5aeae712eb87530ab5eedb35c4343632
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-27 20:36:00 +0800

    login

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -5,14 +5,27 @@ function Login () {
     const lottieTarget = useRef<HTMLDivElement>(null)
     useLottie(lottieTarget.current, animationData)
     return (
-        <div className='w-full h-full bg-gradient-to-b from-cyan-500 to-blue-500 flex'>
+        <div className='w-full h-full bg-gradient-to-b from-violet-500 to-fuchsia-500 flex'>
             <div className='h-full flex items-center justify-center flex-[2_2] bg-cover bg-center bg-[url("//drawcdn.liuyongzhi.cn/login-bg.jpg")] drop-shadow'>
             </div>
             <div className='h-full flex-1 flex items-center justify-center'>
-                <div className='shadow-2xl w-96 h-96 bg-white rounded-lg'>
-                    <div className='w-[200px] h-[200px] ' ref={lottieTarget} />
-                    <label>项目名称:</label>
-                    <input type="text"/>
+                <div className='w-96 h-auto bg-transparent rounded-lg flex flex-col justify-center'>
+                    <div className='text-[24px] font-[400] font-mono not-italic leading-10 text-center text-cyan-50'>
+                    登录涂鸦
+                    </div>
+                    <label className='mb-[10px]'>邮箱:</label>
+                    <input
+                     placeholder='请输入邮箱'
+                     className='w-full h-[32px] px-1 m-0 bg-inherit focus:outline-[#000] outline outline-2  outline-offset-2 rounded-lg placeholder:text-[#BCBCBC]'
+                    />
+                    <label className='mb-[10px] mt-[10px]'>密码:</label>
+                    <input
+                    type="password"
+                     placeholder='请输入密码'
+                     className='w-full h-[32px] px-1 m-0 bg-inherit focus:outline-[#000] outline outline-2  outline-offset-2 rounded-lg  placeholder:text-[#BCBCBC]'
+                    />
+                    <button className='outline-0 point mt-[20px] h-14 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-lg text-slate-100'>登录</button>
+                    <div className='w-[200px] h-[100px] m-auto ' ref={lottieTarget} />
                 </div>
             </div>
         </div>
