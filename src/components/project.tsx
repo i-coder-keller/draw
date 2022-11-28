dde9f8b2133366f340b1bc266bf459974f0478a6
Yong-zhi <yongzhi1.liu@gmail.com>
2022-11-28 22:56:47 +0800

    project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -2,9 +2,40 @@
 function Project() {
 
     return (
-        <div className="w-full h-fit min-h-[100px] p-2 drop-shadow-md hover:drop-shadow-2xl cursor-pointer rounded border-[1px] border-solid border-slate-200">
-            project
-        </div>
+        <section className="w-full h-fit min-h-[100px] p-2 shadow-md hover:shadow-2xl cursor-pointer rounded border-[1px] border-solid border-slate-200">
+            <h1 className="text-sm text-slate-800">测试项目</h1>
+            <div className="w-full h-fit">
+                <div className="flex items-center justify-start my-1">
+                    <div className="relative">
+                        <img src="http://drawcdn.liuyongzhi.cn/default-avatar.png" alt="avatar" className="w-[15px] h-[15px] rounded-full" />
+                        <div className={`absolute w-[5px] h-[5px] rounded-full z-[100] bg-green-500 top-[1px] right-[1px]`}></div>
+                    </div>
+                    <div className="ml-[5px] text-xs text-slate-300">放牛娃</div>
+                </div>
+                <div className="flex items-center justify-start my-1">
+                    <div className="relative">
+                        <img src="http://drawcdn.liuyongzhi.cn/default-avatar.png" alt="avatar" className="w-[15px] h-[15px] rounded-full" />
+                        <div className={`absolute w-[5px] h-[5px] rounded-full z-[100] bg-green-500 top-[1px] right-[1px]`}></div>
+                    </div>
+                    <div className="ml-[5px] text-xs text-slate-300">放牛娃</div>
+                </div>
+                <div className="flex items-center justify-start my-1">
+                    <div className="relative">
+                        <img src="http://drawcdn.liuyongzhi.cn/default-avatar.png" alt="avatar" className="w-[15px] h-[15px] rounded-full" />
+                        <div className={`absolute w-[5px] h-[5px] rounded-full z-[100] bg-green-500 top-[1px] right-[1px]`}></div>
+                    </div>
+                    <div className="ml-[5px] text-xs text-slate-300">放牛娃</div>
+                </div>
+                <div className="flex items-center justify-start my-1">
+                    <div className="relative">
+                        <img src="http://drawcdn.liuyongzhi.cn/default-avatar.png" alt="avatar" className="w-[15px] h-[15px] rounded-full" />
+                        <div className={`absolute w-[5px] h-[5px] rounded-full z-[100] bg-green-500 top-[1px] right-[1px]`}></div>
+                    </div>
+                    <div className="ml-[5px] text-xs text-slate-300">放牛娃</div>
+                </div>
+            </div>
+            <span className="text-xs text-slate-300">40 分钟前修改</span>
+        </section>
     )
 }
 
